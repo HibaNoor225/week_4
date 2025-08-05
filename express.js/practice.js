c625c6674daf1322c4d37a8b95bb156a8274b8de
Hiba <hiba15225@gmail.com>
2025-08-05 10:47:06 +0500

    Initial Commit

--- express.js/practice.js
+++ express.js/practice.js
@@ -1,6 +1,7 @@
 //---------------------Basic get route---------------------
 // const express = require('express');
-// const app = express();
+// const app = express();   
+//
 // app.get('/', (req, res) => {
 //     res.send('Welcome to the Express.js Tutorial');
 // });
